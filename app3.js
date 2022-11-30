c782ee2a96fb021a252b7423df11294a34ad2bde
CJ Evans <cje.software.engineer@gmail.com>
2022-11-30 10:18:30 -0600

    added styling for app3 mvmnt optimized for machine perf.

--- app3.js
+++ app3.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 //initial JavaScript setup for canvas animation and Game Development *****END*****
-const numberOfEnemies = 10;
+const numberOfEnemies = 144;
 const enemiesArray = [];
 let gameFrame = 0;
 
@@ -19,18 +19,19 @@ class Enemy {
         this.speed = Math.random() * 4 + 1;
         this.spriteWidth = 218;
         this.spriteHeight = 177;
-        this.width = this.spriteWidth / 3;
-        this.height = this.spriteHeight / 3;
+        this.width = this.spriteWidth / 2;
+        this.height = this.spriteHeight / 2;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 2;
-        this.curve = Math.random() * 8;
+        this.angle = 0;//Math.random() * 333;<<<enemies will show up randomly in trajectory path
+        this.angleSpeed = Math.random() * 0.5 + 0.5;
+        //this.curve = Math.random() * 200 + 50;
     }
     update() {
-        this.x = 100 * Math.sin(this.angle * Math.PI / 180) + 100;
+        this.x = canvas.width / 2 * Math.cos(this.angle * Math.PI / 200) + (canvas.width / 2 - this.width / 2);
+        this.y = canvas.height / 2  * Math.sin(this.angle * Math.PI / 300) + (canvas.height / 2 - this.height / 2);
         //this.x -= this.speed;
         //this.y += this.curve * Math.sin(this.angle);
         this.angle += this.angleSpeed;
@@ -65,3 +66,9 @@ function animate() {
 
 animate();
 //remember if you want to use this version of 'app3.js' you must change the body script src value to app2.js
+//the above trig code for AI mvmnt is PRICELESS for coding animations particale effects...EVERYTHING ANIMATION!
+/*
+this.x = canvas.width / 2 * Math.cos(this.angle * Math.PI / 200) + (canvas.width / 2 - this.width / 2);
+this.y = canvas.height / 2  * Math.sin(this.angle * Math.PI / 300) + (canvas.height / 2 - this.height / 2);
+trying changing the meriad of values in these two update() method constructors to create figure 8 patterns a circle and more
+*/
